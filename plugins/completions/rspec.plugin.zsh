d4da9c6e1bcd20d05787eec9bbdeebc03a936aa8
Andreas Bade <andreas.bade@identpro.de>
2017-01-23 15:01:22 +0100

    works much better like this
    rspec:compadd:1: can only be called from completion function

--- plugins/completions/rspec.plugin.zsh
+++
@@ -1,7 +0,0 @@
-#compdef rspec
-# rspec autocompletion by git status
-function __rspec() {
-  compadd -J changes    -X "with git changes" -x "-- with git changes --" -P spec/ $(git status | grep "spec/" | sed "s/.*:\s*spec\///g")
-  compadd -J no_changes -X "no changes"       -x "-- no changes --"       -P spec/ $(ls spec/**/*_spec.rb | sed -E "s/spec\///g")
-}
-compdef __rspec rspec
