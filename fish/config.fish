a2e981b96938d540f9f049b571d943c67f47e9f2
Andi Bade <andi.bade@gmail.com>
2021-03-12 20:40:24 +0100

    many updates :-)

--- fish/config.fish
+++ fish/config.fish
@@ -10,4 +10,21 @@ alias mux tmuxinator
 
 echo "setze EBI_HOME"
 set -x EBI_HOME /home/andi/Projekte/kbs/ebi
-set -x EDITOR code --wait
+set -x EDITOR codium --wait
+
+alias asciicast2gif 'docker run --rm -v $PWD:/data asciinema/asciicast2gif'
+
+alias bt 'cd /home/andi/Projekte/brautagebuch/brautagebuchapp/'
+
+set -x PATH /home/andi/.yarn/bin $PATH
+
+set -x JAVA_OPTS "-Xms128m -Xmx4096m"
+
+set -x PATH $PATH $HOME/.npm/bin
+
+set -x VAULT_ADDR https://vault.cloud.kbs.de
+
+rvm default
+
+# deactivate capslock
+# gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none']"
