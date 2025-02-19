cbccc60250254b3032fb68c62f6e0b8f54643e9a
Andreas Bade <andi.bade@gmail.com>
2025-02-19 21:28:58 +0100

    make node available for everybody

--- shell/functions.zsh
+++ shell/functions.zsh
@@ -12,3 +12,9 @@ withPassword() {
   echo -n Password:
   read -s password
 }
+
+nodeForSudo() {
+  sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+  sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
+  sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npx" "/usr/local/bin/npx"
+}
