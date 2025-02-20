de71cc6bf63f0996bea7f5dbf870bc6bdc0acabe
Andreas Bade <andi.bade@gmail.com>
2025-02-20 10:04:27 +0100

    install docker credentials helper

--- shell/install-scripts.zsh
+++ shell/install-scripts.zsh
@@ -4,3 +4,9 @@ install-zellij() {
   chmod +x zellij
   sudo mv zellij /home/andi/bin
 }
+
+installDockerCredHelpers() {
+  curl -L https://github.com/docker/docker-credential-helpers/releases/latest/download/docker-credential-pass-v0.8.2.linux-amd64 -o docker-credential-pass
+  chmod +x docker-credential-pass
+  sudo mv docker-credential-pass /home/andi/bin
+}
